6d232e3c1e9124eb399d14a74ee1ae9d4d7ff416
Pongstr <twiz.tickler@gmail.com>
2024-12-13 12:24:42 +0200

    init: fork with updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,75 @@ import trac from "tailwindcss-react-aria-components";
 import contQueries from "@tailwindcss/container-queries";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "system-ui"],
-      },
-      colors: {
-        primary:
-          "light-dark(oklch(49.12% 0.3096 285.75), oklch(65.69% 0.196 285.75))",
-        "primary-content":
-          "light-dark(oklch(0.89824 0.06192 285.75), oklch(0.13138 0.0392 285.75))",
-        secondary:
-          "light-dark(oklch(69.71% 0.329 342.55), oklch(74.8% 0.26 342.55))",
-        accent:
-          "light-dark(oklch(76.76% 0.184 183.61), oklch(74.51% 0.167 183.61))",
-        "base-content": "light-dark(#1f2937, #A6ADBB)",
-        "base-100": "light-dark(oklch(100% 0 0), #1d232a)",
-        "base-200": "light-dark(#F2F2F2, #191e24)",
-        "base-300": "light-dark(#E5E6E6, #15191e)",
-      },
-    },
-
-    fontFamily: {
-      keycap: ["Inter", "system-ui"],
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: [
+  				'Inter',
+  				'system-ui'
+  			]
+  		},
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'primary-content': 'light-dark(oklch(0.89824 0.06192 285.75), oklch(0.13138 0.0392 285.75))',
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			'base-content': 'light-dark(#1f2937, #A6ADBB)',
+  			'base-100': 'light-dark(oklch(100% 0 0), #1d232a)',
+  			'base-200': 'light-dark(#F2F2F2, #191e24)',
+  			'base-300': 'light-dark(#E5E6E6, #15191e)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	},
+  	fontFamily: {
+  		keycap: [
+  			'Inter',
+  			'system-ui'
+  		]
+  	}
   },
-  plugins: [contQueries, trac({ prefix: "rac" })],
+  plugins: [contQueries, trac({ prefix: "rac" }), require("tailwindcss-animate")],
 };
