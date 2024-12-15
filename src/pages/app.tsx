66a752fd4d7b3f0c3709854ba97f5533ae4c2dab
Pongstr <twiz.tickler@gmail.com>
2024-12-15 18:45:00 +0200

    nope, not needed

--- src/pages/app.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react'
-
-const AppPage: FC = () => {
-  return <div></div>
-}
-
-export default AppPage
--- src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react'
-
-const TestPage: FC = () => {
-  return <div>Test</div>
-}
-
-export default TestPage
